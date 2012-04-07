e3cc4dab7b662c1a03c2b208b04a1033af68a63f
Dan Branton <dtotheb@gmail.com>
2012-04-07 18:48:56 -0400

    Setup TestHelper and a basic test for the index view

--- contacts/tests.py
+++ contacts/tests.py
@@ -1,16 +1,34 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.core.urlresolvers import reverse
+from contacts.models import Contact
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+class TestHelper(object):
+
+    def create_contact(self, **kwargs):
         """
-        Tests that 1 + 1 always equals 2.
+        Creates a Contact model in the DB
         """
-        self.assertEqual(1 + 1, 2)
+        defaults = {
+            'name': 'test',
+            'email': 'test@test.com'
+        }
+        defaults.update(kwargs)
+        return Contact.objects.create(**defaults)
+
+
+class index_TestCase(TestHelper, TestCase):
+    """
+    Tests the index view
+    """
+    def setUp(self):
+        TestHelper.create_contact(self)
+        self.url = reverse('index')
+
+    def test_pageLoads(self):
+        """
+        Tests that the page loads, and uses the expected template.
+        """
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'contacts/index.html')
--- contacts/urls.py
+++ contacts/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, url
 
 
 urlpatterns = patterns('',
-    url(r'^$', 'contacts.views.index'),
+    url(r'^$', 'contacts.views.index', name='index'),
     )
