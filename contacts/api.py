d6d7d1c3137956f3d3a91a3cbaae50b1726dddb5
Dan Branton <dtotheb@gmail.com>
2012-04-08 17:00:37 -0400

    Added user to the api & enabled authorization

--- contacts/api.py
+++ contacts/api.py
@@ -1,8 +1,23 @@
 from tastypie.resources import ModelResource
 from contacts.models import Contact
+from django.contrib.auth.models import User
+from tastypie.authorization import Authorization
 
 
 class ContactResource(ModelResource):
     class Meta:
         queryset = Contact.objects.all()
         resource_name = 'contact'
+        authorization = Authorization()
+
+
+class UserResource(ModelResource):
+    class Meta:
+        queryset = User.objects.all()
+        resource_name = 'user'
+        excludes = ['email',
+                    'password',
+                    'is_active',
+                    'is_staff',
+                    'is_superuser']
+        allowed_methods = ['get']
--- contacts/urls.py
+++ contacts/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import patterns, url, include
-from contacts.api import ContactResource
+from contacts.api import ContactResource, UserResource
+from tastypie.api import Api
 
-contact_resource = ContactResource()
+
+v1_api = Api(api_name='v1')
+v1_api.register(UserResource())
+v1_api.register(ContactResource())
 
 urlpatterns = patterns('',
     url(r'^$', 'contacts.views.index', name='index'),
     url(r'^create$', 'contacts.views.create', name='create'),
-    url(r'^api/', include(contact_resource.urls)),
+    url(r'^api/', include(v1_api.urls)),
     )
